08501acc9c1813c121cf8b732b2773ee1e4c64e1
Kevin Ji <kevinandberbiscool@gmail.com>
2025-12-28 22:52:15 -0500

    hyprland tweak hypridle start and qutebrowser start

---
+++ qutebrowser/config.py
@@ -0,0 +1,18 @@
+config.load_autoconfig()
+
+# yt add block from https://github.com/vEnhance/dotfiles/blob/2b6c5a95ca2fe962d7a6c27e9ad3bf1fc7fefa94/qutebrowser/config.py
+
+from qutebrowser.api import interceptor
+
+def filter_youtube(info: interceptor.Request):
+    """Block given request if necessary"""
+    url = info.request_url
+    if (
+        url.host() == "www.youtube.com"
+        and url.path() == "/get_video_info"
+        and "&adformat=" in url.query()
+    ):
+        info.block()
+
+
+interceptor.register(filter_youtube)
